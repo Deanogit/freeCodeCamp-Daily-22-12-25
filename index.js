c5c24e00255d59258dda4ff8b996aba499de2520
Dean Howe <hideanhowe@gmail.com>
2025-12-16 12:38:41 +0000

    testing charCodeAt

--- index.js
+++ index.js
@@ -7,37 +7,24 @@
 // Non-alphabetical characters should not get decoded.
 
 function decode(message, shift) {
-  //console.log(message, shift);
+  console.log(message, shift);
   // reference arr
-  const alpha = [
-    a,
-    b,
-    c,
-    d,
-    e,
-    f,
-    g,
-    h,
-    i,
-    j,
-    k,
-    l,
-    m,
-    n,
-    o,
-    p,
-    q,
-    r,
-    s,
-    t,
-    u,
-    v,
-    w,
-    x,
-    y,
-    z,
-  ];
-  console.log(alpha);
+  // const alpha = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+  // console.log(alpha)
+  let counter = 0;
+  for (let i = 0; i < message.length; i++) {
+    // console.log(i)
+    const regex = /[a-zA-Z]/;
+    if (regex.test(message[i])) {
+      console.log(message[i].charCodeAt(0));
+      counter = message[i].charCodeAt(0);
+      //if (shift > 0) {
+      counter = counter - shift;
+      //} else {
+      // counter = counter + shift;
+      //}
+    }
+  }
   // return message;
   // shift each letter according to shift
 }
