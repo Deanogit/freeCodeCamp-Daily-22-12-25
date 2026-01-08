2f79a448611ed5db24f8af23907fd1d7f479100e
Dean Howe <hideanhowe@gmail.com>
2026-01-08 17:29:48 +0000

    testing conv helper

--- index.js
+++ index.js
@@ -13,3 +13,30 @@
 // CAD	0.75 USD
 // If you can afford all the items in the list, return "Buy them all!".
 // Otherwise, return "Buy the first X items.", where X is the number of items you can afford when purchased in the order given.
+
+function buyItems(funds, items) {
+  console.log(funds, items);
+
+  const convTable = {
+    USD: 1,
+    EUR: 1.1,
+    GBP: 1.25,
+    JPY: 0.007,
+    CAD: 0.75,
+  };
+
+  // build a converter helper
+  function converter(value, curr) {
+    if (curr !== 'USD') {
+      let convRate = convTable[curr].value;
+      // convert value
+      console.log(convRate);
+      return value * convRate;
+    }
+    return value;
+  }
+
+  converter(100, 'USD');
+
+  return funds;
+}
